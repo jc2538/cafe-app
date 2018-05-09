74dbd6255369c037e5797dceb3a6d21c29c3469a
Jessica Chen <jessicachen@Jessicas-MacBook-Pro.local>
2018-05-08 22:58:26 -0400

    Got rid of file_cache warning

--- bookshelf/ml_model.py
+++ bookshelf/ml_model.py
@@ -18,21 +18,21 @@ def predict_json(project, model, instances, version=None):
     """
     # Create the ML Engine service object.
     # To authenticate set the environment variable
-    logging.basicConfig(filename='ml_model.log', level=logging.INFO)
-    logging.info('inside')
-    logging.info(project)
-    logging.info(model)
-    logging.info(instances)
-    logging.info(version)
+    # logging.basicConfig(filename='ml_model.log', level=logging.INFO)
+    # logging.info('inside')
+    # logging.info(project)
+    # logging.info(model)
+    # logging.info(instances)
+    # logging.info(version)
     GOOGLE_APPLICATION_CREDENTIALS='cafe-app-f9f9134f1cd3.json'
-    service = googleapiclient.discovery.build('ml', 'v1')
-    logging.info('cred')
+    service = googleapiclient.discovery.build('ml', 'v1', cache_discovery=False)
+    # logging.info('cred')
     name = 'projects/{}/models/{}'.format(project, model)
-    logging.info('name')
+    # logging.info('name')
 
     if version is not None:
         name += '/versions/{}'.format(version)
-    logging.info('version')
+    # logging.info('version')
 
     #instances = [{"location_id": 0, "hour": 7, "minute": 30, "total_minutes": 420}]
     print("instances here")
