d65206afab30a7b267108898626651b620bb47b9
ec587 <ec587@cornell.edu>
2018-05-12 00:54:51 -0400

    cleaning

--- bookshelf/__init__.py
+++ bookshelf/__init__.py
@@ -17,10 +17,6 @@ import logging
 
 from flask import current_app, Flask, redirect, request, session, url_for
 import httplib2
-# from oauth2client.contrib.flask_util import UserOAuth2
-
-
-# oauth2 = UserOAuth2()
 
 
 def create_app(config, debug=False, testing=False, config_overrides=None):
@@ -108,22 +104,3 @@ def get_model():
             "Please specify datastore, cloudsql, or mongodb")
 
     return model
-
-
-# def _request_user_info(credentials):
-#     """
-#     Makes an HTTP request to the Google+ API to retrieve the user's basic
-#     profile information, including full name and photo, and stores it in the
-#     Flask session.
-#     """
-#     http = httplib2.Http()
-#     credentials.authorize(http)
-#     resp, content = http.request(
-#         'https://www.googleapis.com/plus/v1/people/me')
-
-#     if resp.status != 200:
-#         current_app.logger.error(
-#             "Error while obtaining user profile: \n%s: %s", resp, content)
-#         return None
-
-#     session['profile'] = json.loads(content.decode('utf-8'))
